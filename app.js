b4dd7da10d916a5f497c9d2a6e57ce95258e0d23
alex.fan <alex.fan@okchem.com>
2017-12-30 12:24:36 +0800

    startup

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/js', express.static('public/javascripts'))
+app.use('/css', express.static('public/stylesheets'))
+app.use('/css/images', express.static('public/images'))
 
 app.use('/', index);
 
