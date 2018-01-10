bb565518777d7061b7fd4d4f25fcf95cac8668ce
alex.fan <alex.fan@okchem.com>
2018-01-10 20:10:18 +0800

    step 3 drag and drop - 1st part

--- public/javascripts/carDealer2.js
+++ public/javascripts/carDealer2.js
@@ -29,6 +29,12 @@ var CAR_BOOTH_TEMPLATE =
     '   </div>' +
     '</div>';
 
+// 展位客户模板
+var CLIENT_IN_CAR_BOOTH_TEMPLATE =
+    '<div class="client_in_car_booth" data-car-reception-desk-id="{0}" data-target-brand="{1}">' +
+    '   <img src="{2}" alt=""/>' +
+    '</div>';
+
 // 汽车在展位上的状态
 window.cars_in_place_status = {
     empty: 'EMPTY',
@@ -217,6 +223,14 @@ var carsPlace = (function () {
         return content;
     }
 
+    // 根据 receptionDeskId 获取 CarInPlace in CarsInPlace
+    function _getCarInPlace(receptionDeskId) {
+        var match = window.car_dealer.cars_in_place.filter(function (carInPlace) {
+            return carInPlace.id === receptionDeskId;
+        });
+        return match && match.length > 0 ? match[0] : null;
+    }
+
     // 显示不同品牌汽车的展位区域
     function _renderCarsPlace() {
         Object.keys(window.cars_brand).forEach(function (carKey) {
@@ -234,11 +248,124 @@ var carsPlace = (function () {
         });
     }
 
+    function _isSpecificBrandCarsPlaceAllOccupied(brand) {
+        return window.car_dealer.cars_in_place.filter(function (carInPlace) {
+            return carInPlace.brand === brand;
+        }).filter(function (carInPlace) {
+            return carInPlace.status === window.cars_in_place_status.empty;
+        }).length === 0;
+    }
+
+    function _isClientDroppable(actualBrand, receptionDeskId) {
+        var carInPlace = _getCarInPlace(receptionDeskId);
+        if(carInPlace.brand !== actualBrand &&
+            _isSpecificBrandCarsPlaceAllOccupied(actualBrand))
+            return true;
+        if(carInPlace.status === window.cars_in_place_status.empty &&
+            carInPlace.brand === actualBrand)
+            return true;
+        return false;
+    }
+
+    function _updateCarInPlace(receptionDeskId, client, status) {
+        var carInPlace = _getCarInPlace(receptionDeskId);
+        carInPlace.client = client;
+        carInPlace.status = status;
+    }
+
+    // 在一个展位上显示客户信息
+    function _renderClientInReceptionDesk(receptionDeskId) {
+        var $carReceptionDesk = $('.car_reception_desk[data-car-reception-desk-id=' + receptionDeskId + ']');
+        var client = _getCarInPlace(receptionDeskId).client;
+        $carReceptionDesk.empty();
+        $carReceptionDesk.append(CLIENT_IN_CAR_BOOTH_TEMPLATE.format(
+            receptionDeskId,
+            client.brand,
+            client.img
+        ));
+        var $clientInReceptionDesk = $carReceptionDesk.find('div.client_in_car_booth');
+        $clientInReceptionDesk.draggable({
+            revert: "invalid",
+            cursor: "move"
+        });
+    }
+    function _renderReceptionDeskAfterRemoved(receptionDeskId) {
+        var $carReceptionDesk = $('.car_reception_desk[data-car-reception-desk-id=' + receptionDeskId + ']');
+        $carReceptionDesk.empty();
+        $carReceptionDesk.append('drag and drop client to here');
+    }
+
+    function _renderClientInReceptionDeskOnAdded(clientUI, receptionDeskId) {
+        clientUI.fadeOut('slow', function () {
+            _renderClientInReceptionDesk(receptionDeskId);
+        });
+    }
+    function _renderClientInReceptionDeskOnAddedAndRemoved(clientUI, originalReceptionDeskId, newReceptionDeskId) {
+        clientUI.fadeOut('slow', function () {
+            _renderClientInReceptionDesk(newReceptionDeskId);
+            _renderReceptionDeskAfterRemoved(originalReceptionDeskId);
+        });
+    }
+
+    function _dropClientHandler(event, ui) {
+        // clients queue 里面的 模板 包含 data-client-id 属性
+        // car_reception_desk 里面的 模板 不包含 data-client-id 属性
+        var clientId = ui.draggable.attr('data-client-id');
+        if (clientId) {
+            // 客户来自Clients Queue
+            _updateCarInPlace(
+                $(event.target).attr('data-car-reception-desk-id'),
+                clientsQueue.getClientById(clientId),
+                window.cars_in_place_status.occupied
+            );
+            _renderClientInReceptionDeskOnAdded(
+                ui.draggable,
+                $(event.target).attr('data-car-reception-desk-id')
+            );
+            // 从clientsQueue移除客户数据
+            clientsQueue.removeClient(clientId);
+        } else {
+            // 客户来自同品牌其他展位
+            var originalReceptionDeskId = ui.draggable.attr('data-car-reception-desk-id');
+            var originalCarInPlace = _getCarInPlace(originalReceptionDeskId);
+            var newReceptionDeskId = $(event.target).attr('data-car-reception-desk-id');
+            _updateCarInPlace(
+                newReceptionDeskId,
+                originalCarInPlace.client,
+                window.cars_in_place_status.occupied
+            );
+            _updateCarInPlace(
+                originalReceptionDeskId,
+                null,
+                window.cars_in_place_status.empty
+            );
+            _renderClientInReceptionDeskOnAddedAndRemoved(
+                ui.draggable,
+                originalReceptionDeskId,
+                newReceptionDeskId
+            );
+        }
+    }
+
     // public methods
     function init() {
         _cacheDOM();
         _renderCarsPlace();
         _renderCarsBooth();
+
+        Object.keys(window.cars_brand).forEach(function (carKey) {
+            var $carReceptionDeskList = $('.car_reception_desk'.format(window.cars_brand[carKey]));
+            $carReceptionDeskList.droppable({
+                accept: function(ui) {
+                    var droppedEle = $(this);
+                    return _isClientDroppable(
+                        ui.attr('data-target-brand'),
+                        droppedEle.attr('data-car-reception-desk-id')
+                    );
+                },
+                drop: _dropClientHandler
+            });
+        });
     }
 
     // expose public methods
@@ -266,11 +393,26 @@ var clientsQueue = (function () {
             );
         });
         $clientsQueue.append(content);
+        $(".client").draggable({
+            revert: "invalid",
+            cursor: "move"
+        });
+        // $(".client[data-client-id=" + _client_list[0].id + "]").draggable({
+        //     revert: "invalid",
+        //     cursor: "move"
+        // });
     }
 
     // public methods
     function init() {
         _cacheDOM();
+    }
+
+    function getClientById(clientId) {
+        var match = _client_list.filter(function (client) {
+            return client.id.toString() === clientId;
+        });
+        return match && match.length > 0 ? match[0] : null;
     }
 
     function addClient() {
@@ -285,10 +427,23 @@ var clientsQueue = (function () {
         _renderClietList();
     }
 
+    function removeClient(clientId) {
+        _client_list = _client_list.filter(function (client) {
+            return client.id.toString().toLowerCase() !== clientId;
+        });
+    }
+
+    function getAllClient() {
+        return _client_list;
+    }
+
     // expose public methods
     return {
         init: init,
         addClient: addClient,
+        getClientById: getClientById,
+        removeClient: removeClient,
+        getAllClient: getAllClient,
     };
 })();
 
@@ -309,6 +464,10 @@ $(document).ready(function() {
     clientsQueue.init();
     carsPlace.init();
 
+    clientsQueue.addClient();
+    clientsQueue.addClient();
+    clientsQueue.addClient();
+    clientsQueue.addClient();
     clientsQueue.addClient();
     clientsQueue.addClient();
     clientsQueue.addClient();
